f75b8516da1787987656f0e0f2e1d594553305c7
João Renan <102002118+LoowdY@users.noreply.github.com>
2024-05-25 16:43:52 -0300

    Update indeterministica.py

--- indeterministica.py
+++ indeterministica.py
@@ -1,89 +1,118 @@
 from collections import defaultdict
 import random
 
-class MaquinaDeTuringDuasFitasIndeterministica:
+class MaquinaDeTuringIndeterministica:
     def __init__(self, estados, alfabeto, alfabeto_fita, funcao_transicao, estado_inicial, estado_aceitacao, estado_rejeicao):
         self.estados = estados
         self.alfabeto = alfabeto
         self.alfabeto_fita = alfabeto_fita
         self.funcao_transicao = defaultdict(list)
-        for chave, valor in funcao_transicao.items():
-            self.funcao_transicao[chave].append(valor)
+        for chave, valores in funcao_transicao.items():
+            for valor in valores:
+                self.funcao_transicao[chave].append(valor)
         self.estado_inicial = estado_inicial
         self.estado_aceitacao = estado_aceitacao
         self.estado_rejeicao = estado_rejeicao
-        self.fita1 = []
-        self.fita2 = []
-        self.cabeca1 = 0
-        self.cabeca2 = 0
+        self.fita = []
+        self.cabeca = 0
         self.estado_atual = estado_inicial
 
-    def inicializar_fitas(self, cadeia_entrada):
-        self.fita1 = list(cadeia_entrada) + ['_']
-        self.fita2 = ['_'] * len(self.fita1)
-        self.cabeca1 = 0
-        self.cabeca2 = 0
+    def inicializar_fita(self, cadeia_entrada):
+        self.fita = list(cadeia_entrada) + ['_']
+        self.cabeca = 0
         self.estado_atual = self.estado_inicial
 
     def passo(self):
         if self.estado_atual in [self.estado_aceitacao, self.estado_rejeicao]:
             return
 
-        simbolo_fita1 = self.fita1[self.cabeca1]
-        simbolo_fita2 = self.fita2[self.cabeca2]
-        chave = (self.estado_atual, simbolo_fita1, simbolo_fita2)
+        simbolo_atual = self.fita[self.cabeca]
+        chave = (self.estado_atual, simbolo_atual)
 
         if chave in self.funcao_transicao:
             transicoes_possiveis = self.funcao_transicao[chave]
             transicao_escolhida = random.choice(transicoes_possiveis)
-            novo_estado, novo_simbolo_fita1, novo_simbolo_fita2, movimento1, movimento2 = transicao_escolhida
-            self.fita1[self.cabeca1] = novo_simbolo_fita1
-            self.fita2[self.cabeca2] = novo_simbolo_fita2
+            novo_estado, novo_simbolo, movimento = transicao_escolhida
+            self.fita[self.cabeca] = novo_simbolo
             self.estado_atual = novo_estado
 
-            self.cabeca1 += 1 if movimento1 == 'D' else -1
-            self.cabeca2 += 1 if movimento2 == 'D' else -1
+            self.cabeca += 1 if movimento == 'D' else -1
 
-            if self.cabeca1 < 0:
-                self.cabeca1 = 0
-                self.fita1.insert(0, '_')
-            elif self.cabeca1 >= len(self.fita1):
-                self.fita1.append('_')
-
-            if self.cabeca2 < 0:
-                self.cabeca2 = 0
-                self.fita2.insert(0, '_')
-            elif self.cabeca2 >= len(self.fita2):
-                self.fita2.append('_')
+            if self.cabeca < 0:
+                self.cabeca = 0
+                self.fita.insert(0, '_')
+            elif self.cabeca >= len(self.fita):
+                self.fita.append('_')
 
     def executar(self, cadeia_entrada):
-        self.inicializar_fitas(cadeia_entrada)
+        self.inicializar_fita(cadeia_entrada)
         while self.estado_atual not in [self.estado_aceitacao, self.estado_rejeicao]:
             self.passo()
         return self.estado_atual == self.estado_aceitacao
 
+    def imprimir_estado_final(self):
+        print("Estado final:", self.estado_atual)
+        print("Conteúdo da fita:", ''.join(self.fita))
+        print("Posição da cabeça:", self.cabeca)
+
 # Exemplo: Máquina de Turing Indeterminística para Verificar Palíndromos
-estados = {'q0', 'q1', 'q2', 'q_aceitar', 'q_rejeitar'}
+estados = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q_aceitar', 'q_rejeitar'}
 alfabeto = {'a', 'b', 'c'}
 alfabeto_fita = {'a', 'b', 'c', '_'}
 funcao_transicao = {
-    ('q0', 'a', '_'): ('q1', '_', 'a', 'D', 'E'),
-    ('q0', 'b', '_'): ('q1', '_', 'b', 'D', 'E'),
-    ('q0', 'c', '_'): ('q1', '_', 'c', 'D', 'E'),
-    ('q1', 'a', '_'): ('q1', 'a', '_', 'D', 'E'),
-    ('q1', 'b', '_'): ('q1', 'b', '_', 'D', 'E'),
-    ('q1', 'c', '_'): ('q1', 'c', '_', 'D', 'E'),
-    ('q1', '_', '_'): ('q2', '_', '_', 'E', 'E'),
-    ('q2', 'a', 'a'): ('q2', '_', '_', 'E', 'E'),
-    ('q2', 'b', 'b'): ('q2', '_', '_', 'E', 'E'),
-    ('q2', 'c', 'c'): ('q2', '_', '_', 'E', 'E'),
-    ('q2', '_', '_'): ('q_aceitar', '_', '_', 'N', 'N'),
+    ('q0', 'a'): [('q1', '_', 'D'), ('q2', '_', 'D')],
+    ('q0', 'b'): [('q3', '_', 'D'), ('q4', '_', 'D')],
+    ('q0', 'c'): [('q5', '_', 'D'), ('q6', '_', 'D')],
+    ('q0', '_'): [('q_aceitar', '_', 'N')],
+
+    ('q1', 'a'): [('q1', 'a', 'D')],
+    ('q1', 'b'): [('q1', 'b', 'D')],
+    ('q1', 'c'): [('q1', 'c', 'D')],
+    ('q1', '_'): [('q7', '_', 'E')],
+
+    ('q2', 'a'): [('q2', 'a', 'D')],
+    ('q2', 'b'): [('q2', 'b', 'D')],
+    ('q2', 'c'): [('q2', 'c', 'D')],
+    ('q2', '_'): [('q8', '_', 'E')],
+
+    ('q3', 'a'): [('q3', 'a', 'D')],
+    ('q3', 'b'): [('q3', 'b', 'D')],
+    ('q3', 'c'): [('q3', 'c', 'D')],
+    ('q3', '_'): [('q9', '_', 'E')],
+
+    ('q4', 'a'): [('q4', 'a', 'D')],
+    ('q4', 'b'): [('q4', 'b', 'D')],
+    ('q4', 'c'): [('q4', 'c', 'D')],
+    ('q4', '_'): [('q10', '_', 'E')],
+
+    ('q5', 'a'): [('q5', 'a', 'D')],
+    ('q5', 'b'): [('q5', 'b', 'D')],
+    ('q5', 'c'): [('q5', 'c', 'D')],
+    ('q5', '_'): [('q11', '_', 'E')],
+
+    ('q6', 'a'): [('q6', 'a', 'D')],
+    ('q6', 'b'): [('q6', 'b', 'D')],
+    ('q6', 'c'): [('q6', 'c', 'D')],
+    ('q6', '_'): [('q_aceitar', '_', 'N')],
+
+    ('q7', 'a'): [('q0', '_', 'D')],
+    ('q8', 'b'): [('q0', '_', 'D')],
+    ('q9', 'c'): [('q0', '_', 'D')],
+    ('q10', 'b'): [('q0', '_', 'D')],
+    ('q11', 'c'): [('q0', '_', 'D')],
 }
+
 estado_inicial = 'q0'
 estado_aceitacao = 'q_aceitar'
 estado_rejeicao = 'q_rejeitar'
 
-mt_indeterministica = MaquinaDeTuringDuasFitasIndeterministica(estados, alfabeto, alfabeto_fita, funcao_transicao, estado_inicial, estado_aceitacao, estado_rejeicao)
-cadeia_entrada = "abcba"
-resultado = mt_indeterministica.executar(cadeia_entrada)
-print("Resultado (Palíndromo Indeterminística):", resultado)
+mt = MaquinaDeTuringIndeterministica(estados, alfabeto, alfabeto_fita, funcao_transicao, estado_inicial, estado_aceitacao, estado_rejeicao)
+
+cadeia_entrada = "abccba"
+resultado = mt.executar(cadeia_entrada)
+if resultado:
+    print("A cadeia foi aceita.")
+else:
+    print("A cadeia foi rejeitada.")
+
+mt.imprimir_estado_final()
