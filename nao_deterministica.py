824fffd7abcada84974e865fb106987f3c7bd038
João Renan <102002118+LoowdY@users.noreply.github.com>
2024-05-25 16:03:44 -0300

    Update nao_deterministica.py

--- nao_deterministica.py
+++ nao_deterministica.py
@@ -1,4 +1,4 @@
-class MaquinaDeTuringND:
+class MaquinaDeTuringDuasFitas:
     def __init__(self, estados, simbolos_entrada, simbolos_fita, transicoes, estado_inicial, simbolo_branco, estados_finais):
         self.estados = estados
         self.simbolos_entrada = simbolos_entrada
@@ -7,21 +7,26 @@ class MaquinaDeTuringND:
         self.estado_inicial = estado_inicial
         self.simbolo_branco = simbolo_branco
         self.estados_finais = estados_finais
-        self.fita = []
+        self.fita1 = []
+        self.fita2 = []
         self.estado_atual = estado_inicial
-        self.posicao_cabeca = 0
+        self.posicao_cabeca1 = 0
+        self.posicao_cabeca2 = 0
 
-    def inicializar_fita(self, entrada):
-        self.fita = list(entrada) + [self.simbolo_branco]
-        self.posicao_cabeca = 0
+    def inicializar_fitas(self, entrada):
+        self.fita1 = list(entrada) + [self.simbolo_branco]
+        self.fita2 = [self.simbolo_branco] * (len(entrada) + 1)
+        self.posicao_cabeca1 = 0
+        self.posicao_cabeca2 = 0
         self.estado_atual = self.estado_inicial
 
     def passo(self):
-        simbolo_atual = self.fita[self.posicao_cabeca]
-        if (self.estado_atual, simbolo_atual) in self.transicoes:
-            transicoes_possiveis = self.transicoes[(self.estado_atual, simbolo_atual)]
-            for estado_proximo, simbolo_escrever, direcao in transicoes_possiveis:
-                nova_maquina = MaquinaDeTuringND(
+        simbolo_atual1 = self.fita1[self.posicao_cabeca1]
+        simbolo_atual2 = self.fita2[self.posicao_cabeca2]
+        if (self.estado_atual, simbolo_atual1, simbolo_atual2) in self.transicoes:
+            transicoes_possiveis = self.transicoes[(self.estado_atual, simbolo_atual1, simbolo_atual2)]
+            for estado_proximo, simbolo_escrever1, simbolo_escrever2, direcao1, direcao2 in transicoes_possiveis:
+                nova_maquina = MaquinaDeTuringDuasFitas(
                     self.estados,
                     self.simbolos_entrada,
                     self.simbolos_fita,
@@ -30,19 +35,22 @@ class MaquinaDeTuringND:
                     self.simbolo_branco,
                     self.estados_finais
                 )
-                nova_maquina.fita = self.fita[:]
-                nova_maquina.fita[self.posicao_cabeca] = simbolo_escrever
-                nova_maquina.posicao_cabeca = self.posicao_cabeca + (1 if direcao == 'D' else -1)
+                nova_maquina.fita1 = self.fita1[:]
+                nova_maquina.fita2 = self.fita2[:]
+                nova_maquina.fita1[self.posicao_cabeca1] = simbolo_escrever1
+                nova_maquina.fita2[self.posicao_cabeca2] = simbolo_escrever2
+                nova_maquina.posicao_cabeca1 = self.posicao_cabeca1 + (1 if direcao1 == 'D' else -1)
+                nova_maquina.posicao_cabeca2 = self.posicao_cabeca2 + (1 if direcao2 == 'D' else -1)
                 yield nova_maquina
 
     def executar(self, entrada):
-        self.inicializar_fita(entrada)
+        self.inicializar_fitas(entrada)
         configuracoes = [self]
 
         while configuracoes:
             configuracao_atual = configuracoes.pop()
             if configuracao_atual.estado_atual in self.estados_finais:
-                print("Aceito: ", ''.join(configuracao_atual.fita).strip(self.simbolo_branco))
+                print("Aceito: ", ''.join(configuracao_atual.fita1).strip(self.simbolo_branco))
                 return True
 
             for proxima_configuracao in configuracao_atual.passo():
@@ -51,40 +59,41 @@ class MaquinaDeTuringND:
         print("Rejeitado")
         return False
 
-# Definição da Máquina de Turing Não Determinística com 12 estados
+# Definição da Máquina de Turing com duas fitas e 12 estados
 estados = {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'qf'}
 simbolos_entrada = {'0', '1'}
 simbolos_fita = {'0', '1', '_'}
 transicoes = {
-    ('q0', '0'): {('q1', '0', 'D'), ('q2', '1', 'D')},
-    ('q0', '1'): {('q3', '1', 'D')},
-    ('q1', '0'): {('q4', '0', 'D')},
-    ('q1', '1'): {('q5', '1', 'D')},
-    ('q2', '0'): {('q6', '0', 'D')},
-    ('q2', '1'): {('q7', '1', 'D')},
-    ('q3', '0'): {('q8', '0', 'D')},
-    ('q3', '1'): {('q9', '1', 'D')},
-    ('q4', '0'): {('q10', '0', 'D')},
-    ('q5', '1'): {('q11', '1', 'D')},
-    ('q6', '0'): {('qf', '0', 'D')},
-    ('q7', '1'): {('qf', '1', 'D')},
-    ('q8', '0'): {('qf', '0', 'D')},
-    ('q9', '1'): {('qf', '1', 'D')},
-    ('q10', '0'): {('qf', '0', 'D')},
-    ('q11', '1'): {('qf', '1', 'D')}
+    ('q0', '0', '_'): {('q1', '0', '0', 'D', 'D'), ('q2', '0', '1', 'D', 'D')},
+    ('q0', '1', '_'): {('q3', '1', '0', 'D', 'D')},
+    ('q1', '0', '_'): {('q4', '0', '0', 'D', 'D')},
+    ('q1', '1', '_'): {('q5', '1', '1', 'D', 'D')},
+    ('q2', '0', '_'): {('q6', '0', '0', 'D', 'D')},
+    ('q2', '1', '_'): {('q7', '1', '1', 'D', 'D')},
+    ('q3', '0', '_'): {('q8', '0', '0', 'D', 'D')},
+    ('q3', '1', '_'): {('q9', '1', '1', 'D', 'D')},
+    ('q4', '0', '_'): {('q10', '0', '0', 'D', 'D')},
+    ('q5', '1', '_'): {('q11', '1', '1', 'D', 'D')},
+    ('q6', '0', '_'): {('qf', '0', '0', 'D', 'D')},
+    ('q7', '1', '_'): {('qf', '1', '1', 'D', 'D')},
+    ('q8', '0', '_'): {('qf', '0', '0', 'D', 'D')},
+    ('q9', '1', '_'): {('qf', '1', '1', 'D', 'D')},
+    ('q10', '0', '_'): {('qf', '0', '0', 'D', 'D')},
+    ('q11', '1', '_'): {('qf', '1', '1', 'D', 'D')}
 }
 estado_inicial = 'q0'
 simbolo_branco = '_'
 estados_finais = {'qf'}
 
-maquina = MaquinaDeTuringND(estados, simbolos_entrada, simbolos_fita, transicoes, estado_inicial, simbolo_branco, estados_finais)
+maquina = MaquinaDeTuringDuasFitas(estados, simbolos_entrada, simbolos_fita, transicoes, estado_inicial, simbolo_branco, estados_finais)
 
 # Exemplo 1
 entrada1 = "010101"
-print(f"Executando a Máquina de Turing ND com entrada: {entrada1}")
+print(f"Executando a Máquina de Turing ND com duas fitas com entrada: {entrada1}")
 maquina.executar(entrada1)
 
 # Exemplo 2
 entrada2 = "111000"
-print(f"Executando a Máquina de Turing ND com entrada: {entrada2}")
+print(f"Executando a Máquina de Turing ND com duas fitas com entrada: {entrada2}")
 maquina.executar(entrada2)
+
